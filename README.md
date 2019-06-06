afe24648293398d326c2632419f79b8a2450c1b2
shuhei.takahashi <uhiaha888@gmail.com>
2019-06-06 23:43:23 +0900

    ECHOモードをOFFにする

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ```
 sudo apt install libncurses5-dev
+
+go get github.com/tncardoso/gcurses
+go get github.com/pkg/term
 ```
--- main.go
+++ main.go
@@ -3,11 +3,51 @@ package main
 import (
 	"bufio"
 	"io"
+	"github.com/pkg/term/termios"
 	"os"
-	"fmt"
+	"syscall"
 )
 
+func disableRawMode(term *syscall.Termios) error {
+	err := termios.Tcsetattr(uintptr(syscall.Stdin), termios.TCSAFLUSH, term)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func enableRawMode(term *syscall.Termios) error {
+	newTerm := *term
+	err := termios.Tcgetattr(uintptr(syscall.Stdin), &newTerm)
+	if err != nil {
+		return err
+	}
+	newTerm.Lflag &^= syscall.ECHO
+	if err = termios.Tcsetattr(uintptr(syscall.Stdin), termios.TCSAFLUSH, &newTerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getTerm() (*syscall.Termios, error) {
+	var term syscall.Termios
+	err := termios.Tcgetattr(uintptr(syscall.Stdin), &term)
+	if err != nil {
+		return nil, err
+	}
+	return &term, nil
+}
+
 func main() {
+	var term, err = getTerm()
+	if err != nil {
+		return
+	}
+	err = enableRawMode(term)
+	defer disableRawMode(term)
+	if err != nil {
+		return
+	}
 	stdin := bufio.NewReader(os.Stdin)
 	for {
 		ch, err := stdin.ReadByte()
@@ -15,6 +55,8 @@ func main() {
 			break
 		}
 		r := rune(ch)
-		fmt.Printf("AAA %c\n", r)
+		if r == 'q' {
+			break
+		}
 	}
 }
