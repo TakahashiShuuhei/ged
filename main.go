f019656d2ce4d8a7df95eec56240501c6b64bf78
shuhei.takahashi <uhiaha888@gmail.com>
2019-06-06 23:57:38 +0900

    Disable C-s and C-q

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func enableRawMode(term *syscall.Termios) error {
 	if err != nil {
 		return err
 	}
+	newTerm.Iflag &^= syscall.IXON
 	newTerm.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.ISIG
 	if err = termios.Tcsetattr(uintptr(syscall.Stdin), termios.TCSAFLUSH, &newTerm); err != nil {
 		return err
